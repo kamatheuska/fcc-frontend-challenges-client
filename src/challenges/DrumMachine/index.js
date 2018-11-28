f2b963fa0af413ad898b1ec6b99ba5e97f943822
Nicolas Ramirez <nicolas.ramirez.ka@gmail.com>
2018-11-28 15:54:41 +0100

    Added folder to challenges, DrumMachine, with started component

---
+++ src/challenges/DrumMachine/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default class ComponentName extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            FOO
+        }
+    }
+
+    componentDidMount () {
+
+    }
+
+    componentDidUpdate () {
+
+    }
+
+    render () {
+        return (
+            <div className="ComponentName"></div>
+        )
+    }
+}
