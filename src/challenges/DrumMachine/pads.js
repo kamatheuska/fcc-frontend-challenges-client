638a77386304ffc39f30a787c10419568de46e61
Nicolas Ramirez <nicolas.ramirez.ka@gmail.com>
2018-12-10 16:37:48 +0100

    Created separate file for pads data.

---
+++ src/challenges/DrumMachine/pads.js
@@ -0,0 +1,48 @@
+export default [
+	{ 
+		id: 'sampleaudio', 
+		innerText: 'Q',
+		audioSrc: 'someSource'
+	},
+	{ 
+		id: 'sampleaudio', 
+		innerText: 'W',
+		audioSrc: 'someSource'
+	},
+	{ 
+		id: 'sampleaudio', 
+		innerText: 'E',
+		audioSrc: 'someSource'
+	},
+	{ 
+		id: 'sampleaudio', 
+		innerText: 'A',
+		audioSrc: 'someSource'
+	},
+	{ 
+		id: 'sampleaudio', 
+		innerText: 'S',
+		audioSrc: 'someSource'
+	},
+	{ 
+		id: 'sampleaudio', 
+		innerText: 'D',
+		audioSrc: 'someSource'
+	},
+	{ 
+		id: 'sampleaudio', 
+		innerText: 'Z',
+		audioSrc: 'someSource'
+	},
+	{ 
+		id: 'sampleaudio', 
+		innerText: 'X',
+		audioSrc: 'someSource'
+	},
+	{ 
+		id: 'sampleaudio', 
+		innerText: 'C',
+		audioSrc: 'someSource'
+	}
+]
+
